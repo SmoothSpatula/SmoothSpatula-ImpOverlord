afb33c9cdd9b5d5fa645fe25a9cf59ccbcb03c61
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-06-15 10:53:29 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,6 +7,10 @@ mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "tab
 -- local portrait_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "sCandymanPortrait.png")
 -- local portraitsmall_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "sCandymanPortraitSmall.png")
 
+-- Using a modified version of https://elthen.itch.io/2d-pixel-art-portal-sprites as a placeholder
+-- local special_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites","PurplePortalSpriteSheet192x96.png")
+-- local ball_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites","sCandymanBall.png")
+
 local skills_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "skillsicons.png")
 -- local loadout_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "sCandymanLoadout.png")
 -- local idle_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "sCandymanIdle.png")
@@ -18,29 +22,42 @@ local skills_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "s
 -- local jump_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "sCandymanjump.png")
 -- local jumpfall_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "sCandymanjumpfall.png")
 -- local hit_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "sCandymanhit.png")
--- local empty_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "sCandymanEmpty.png")
 
 -- local palette_path = path.combine(_ENV["!plugins_mod_folder_path"], "Sprites", "candyman_PAL.png")
 
 
 -- local portrait_sprite = gm.sprite_add(portrait_path, 1, false, false, 0, 0)
 -- local portraitsmall_sprite = gm.sprite_add(portraitsmall_path, 1, false, false, 0, 0)
-
 local skills_sprite = gm.sprite_add(skills_path, 4, false, false, 0, 0)
 -- local loadout_sprite = gm.sprite_add(loadout_path, 7, false, false, 100, 5)
--- local idle_sprite = gm.sprite_add(idle_path, 3, false, false, 29, 45)
--- local walk_sprite = gm.sprite_add(walk_path, 2, false, false, 29, 45)
--- local shoot1_sprite = gm.sprite_add(shoot1_path, 7, false, false, 29, 45)
+local idle_sprite = gm.sprite_duplicate(812)
+local walk_sprite = gm.sprite_duplicate(814)
+local attack_sprite = gm.sprite_duplicate(818)
 -- local shoot1_air_sprite = gm.sprite_add(shoot1_air_path, 7, false, false, 29, 45)
--- local shoot2_sprite = gm.sprite_add(shoot2_path, 4, false, false, 29, 45)
--- local death_sprite = gm.sprite_add(death_path, 4, false, false, 29, 10)
+local utility_sprite = gm.sprite_duplicate(822)
+local secondary_sprite = gm.sprite_duplicate(823)
+local special_sprite = gm.sprite_duplicate(826)
 -- local jump_sprite = gm.sprite_add(jump_path, 1, false, false, 29, 45)
 -- local jumpfall_sprite = gm.sprite_add(jumpfall_path, 1, false, false, 29, 45)
 -- local hit_sprite = gm.sprite_add(hit_path, 1, false, false, 29, 45)
--- local empty_sprite = gm.sprite_add(empty_path, 1, false, false, 0, 0)
+
 
 -- local palette_sprite = gm.sprite_add(hit_path, 1, false, false, 0, 0)
 
+gm.sprite_set_offset(idle_sprite, 70, 107)
+gm.sprite_set_speed(idle_sprite, 1, 1)
+gm.sprite_set_offset(attack_sprite, 50, 113)
+gm.sprite_set_speed(attack_sprite, 1, 1)
+gm.sprite_set_offset(walk_sprite, 70, 95)
+gm.sprite_set_speed(walk_sprite, 1, 1)
+gm.sprite_set_offset(utility_sprite, 38, 100)
+gm.sprite_set_speed(utility_sprite, 1, 1)
+gm.sprite_set_offset(secondary_sprite, 50, 113)
+gm.sprite_set_speed(secondary_sprite, 1, 1)
+gm.sprite_set_offset(special_sprite, 50, 110)
+gm.sprite_set_speed(special_sprite, 1, 1)
+
+print(empty_sprite)
 
 -- ========== Survivor Setup ==========
 
@@ -54,6 +71,10 @@ local function create_survivor()
     ImpOverlord_id = gm.survivor_create("SmoothSpatula", "ImpOverlord")
     ImpOverlord = survivor_setup.Survivor(ImpOverlord_id)
 
+    
+
+
+
     -- Configure Properties
     ImpOverlord.token_name = "ImpOverlord"
     ImpOverlord.token_name_upper = "ImpOverlord"
@@ -61,14 +82,14 @@ local function create_survivor()
     ImpOverlord.token_end_quote = "..."
 
     ImpOverlord.sprite_loadout = 832
-    ImpOverlord.sprite_title = 814
-    ImpOverlord.sprite_idle = 812
+    ImpOverlord.sprite_title = walk_sprite
+    ImpOverlord.sprite_idle = idle_sprite
     ImpOverlord.sprite_portrait = 722
     ImpOverlord.sprite_portrait_small = 722
     ImpOverlord.sprite_palette = 830
     ImpOverlord.sprite_portrait_palette = 830
     ImpOverlord.sprite_loadout_palette = 830
-    ImpOverlord.sprite_credits = 814
+    ImpOverlord.sprite_credits = walk_sprite
 
     -- Configure Skills
 
@@ -82,7 +103,7 @@ local function create_survivor()
     skill_primary.sprite = skills_sprite
     skill_primary.subimage = 0
 
-    skill_primary.animation = skills[170][16]
+    skill_primary.animation = attack_sprite
 
     skill_primary.cooldown = 0
     skill_primary.damage = 1.5
@@ -103,7 +124,8 @@ local function create_survivor()
     skill_secondary.sprite = skills_sprite
     skill_secondary.subimage = 1
 
-    skill_secondary.animation = skills[172][16]
+    skill_secondary.animation = secondary_sprite
+    
 
     skill_secondary.cooldown = 0
     skill_secondary.damage = 1.5
@@ -125,11 +147,11 @@ local function create_survivor()
     skill_utility.sprite = skills_sprite
     skill_utility.subimage = 2
 
-    skill_utility.animation = skills[173][16]
+    skill_utility.animation = utility_sprite
 
-    skill_utility.cooldown = 0
+    skill_utility.cooldown = 30
     skill_utility.damage = 1.5
-    skill_utility.required_stock = 0
+    skill_utility.required_stock = 1
     skill_utility.require_key_press = true
     skill_utility.use_delay = 0
     skill_utility.is_primary = false
@@ -148,7 +170,7 @@ local function create_survivor()
     skill_special.sprite = skills_sprite
     skill_special.subimage = 3
 
-    skill_special.animation = skills[174][16]
+    skill_special.animation = special_sprite
 
     skill_special.cooldown = 0
     skill_special.damage = 1.5
@@ -167,19 +189,32 @@ end
 
 --create_survivor()
 
-local function setup_sprites(self)
+local function setup_sprites(self, invis_sprite)
     local survivors = gm.variable_global_get("class_survivor")
 
     if not survivors or self.class ~= ImpOverlord_id then return end
 
-    self.sprite_idle        = 812
-    self.sprite_walk        = 814
-    self.sprite_jump        = 812
-    self.sprite_jump_peak   = 812
-    self.sprite_fall        = 812
-    self.sprite_climb       = 812
-    self.sprite_death       = 828
+    self.sprite_idle        = idle_sprite
+    self.sprite_walk        = walk_sprite
+    self.sprite_jump        = idle_sprite
+    self.sprite_jump_peak   = idle_sprite
+    self.sprite_fall        = idle_sprite
+    self.sprite_climb       = idle_sprite
+    self.sprite_death       = 841
     self.sprite_decoy       = 828
+end
+
+local func_explode =  function (self)
+    gm._mod_attack_fire_explosion(
+        self,
+        self.x,
+        self.y,
+        60,
+        35,
+        self.skills[1].active_skill.damage,
+        gm.constants.sEfBloodExplosion,
+        gm.constants.sSparks1,
+        true)
 end
 
 
@@ -194,17 +229,38 @@ for i = 1, #callback_names do
 end
 
 gm.post_script_hook(gm.constants.callback_execute, function(self, other, result, args)
+    
     if self.class ~= ImpOverlord_id then return end
-
     local callback_id = args[1].value
     if callback_id == on_player_init_callback_id then
         setup_sprites(self)
+    end
+    if callback_id == ImpOverlord.skill_family_z[0].on_activate then
+        -- do better hit
+        func_explode(self)
+        local _handle = gm.call_later(20, 1, gm.constants.function_dummy, false)
+        local _handle2 = gm.call_later(40, 1, gm.constants.function_dummy, false)
+    end
+
+    if callback_id == ImpOverlord.skill_family_c[0].on_activate then
+        -- do better spawn
+        local inst = gm.instance_create_depth(self.x, self.y, self.layer, gm.constants["oImpFriend"])
     end
 end)
 
 gm.post_script_hook(gm.constants.stage_load_room, function(self, other, result, args)
     if not is_init then 
-        print("test")
         is_init = true
         create_survivor() end
+end)
+
+gm.post_script_hook(gm.constants.function_dummy, function(self, other, result, args)
+    func_explode(Helper.get_client_player())
+end)
+
+gm.post_script_hook(gm.constants.texture_flush_group, function()
+    local Imps = Helper.find_active_instance_all(gm.constants.oImpFriend)
+    for _, imp in ipairs(Imps) do
+        gm.instance_destroy(imp)
+    end
 end)
